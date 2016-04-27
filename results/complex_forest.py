a0f66e631d1d03ce566be8e7c7e65f27a5faa650
mhbkb <mao170549392>
2016-04-26 19:15:42 -0700

    With good

--- results/complex_forest.py
+++ results/complex_forest.py
@@ -152,7 +152,7 @@ class cust_regression_vals(BaseEstimator, TransformerMixin):
     def fit(self, x, y=None):
         return self
     def transform(self, hd_searches):
-        d_col_drops=['id','relevance','search_term','product_title','product_description','product_info','brand']
+        d_col_drops=['id','relevance','search_term','product_title','product_description','product_info','attr','brand']
         hd_searches = hd_searches.drop(d_col_drops,axis=1).values
         return hd_searches
 
@@ -164,17 +164,46 @@ class cust_txt_col(BaseEstimator, TransformerMixin):
     def transform(self, data_dict):
         return data_dict[self.key].apply(str)
 
-df_all['search_term'] = df_all['search_term'].map(lambda x:str_stem(x))
-df_all['product_title'] = df_all['product_title'].map(lambda x:str_stem(x))
-df_all['product_description'] = df_all['product_description'].map(lambda x:str_stem(x))
-df_all['brand'] = df_all['brand'].map(lambda x:str_stem(x))
-df_all['product_info'] = df_all['search_term']+"\t"+df_all['product_title'] +"\t"+df_all['product_description']
-df_all['len_of_query'] = df_all['search_term'].map(lambda x:len(x.split())).astype(np.int64)
-df_all['search_term'] = df_all['product_info'].map(lambda x:seg_words(x.split('\t')[0],x.split('\t')[1]))
-df_all['word_in_title'] = df_all['product_info'].map(lambda x:str_common_word(x.split('\t')[0],x.split('\t')[1]))
-df_all['word_in_description'] = df_all['product_info'].map(lambda x:str_common_word(x.split('\t')[0],x.split('\t')[2]))
-df_all.to_csv('data_all.csv')
-df_all = pd.read_csv('data_all.csv', encoding="ISO-8859-1", index_col=0)
+#comment out the lines below use df_all.csv for further grid search testing
+#if adding features consider any drops on the 'cust_regression_vals' class
+#*** would be nice to have a file reuse option or script chaining option on Kaggle Scripts ***
+# df_all['search_term'] = df_all['search_term'].map(lambda x:str_stem(x))
+# df_all['product_title'] = df_all['product_title'].map(lambda x:str_stem(x))
+# df_all['product_description'] = df_all['product_description'].map(lambda x:str_stem(x))
+# df_all['brand'] = df_all['brand'].map(lambda x:str_stem(x))
+# print("--- Stemming: %s minutes ---" % round(((time.time() - start_time)/60),2))
+# df_all['product_info'] = df_all['search_term']+"\t"+df_all['product_title'] +"\t"+df_all['product_description']
+# print("--- Prod Info: %s minutes ---" % round(((time.time() - start_time)/60),2))
+# df_all['len_of_query'] = df_all['search_term'].map(lambda x:len(x.split())).astype(np.int64)
+# df_all['len_of_title'] = df_all['product_title'].map(lambda x:len(x.split())).astype(np.int64)
+# df_all['len_of_description'] = df_all['product_description'].map(lambda x:len(x.split())).astype(np.int64)
+# df_all['len_of_brand'] = df_all['brand'].map(lambda x:len(x.split())).astype(np.int64)
+# print("--- Len of: %s minutes ---" % round(((time.time() - start_time)/60),2))
+# df_all['search_term'] = df_all['product_info'].map(lambda x:seg_words(x.split('\t')[0],x.split('\t')[1]))
+# #print("--- Search Term Segment: %s minutes ---" % round(((time.time() - start_time)/60),2))
+# df_all['query_in_title'] = df_all['product_info'].map(lambda x:str_whole_word(x.split('\t')[0],x.split('\t')[1],0))
+# df_all['query_in_description'] = df_all['product_info'].map(lambda x:str_whole_word(x.split('\t')[0],x.split('\t')[2],0))
+# print("--- Query In: %s minutes ---" % round(((time.time() - start_time)/60),2))
+# df_all['query_last_word_in_title'] = df_all['product_info'].map(lambda x:str_common_word(x.split('\t')[0].split(" ")[-1],x.split('\t')[1]))
+# df_all['query_last_word_in_description'] = df_all['product_info'].map(lambda x:str_common_word(x.split('\t')[0].split(" ")[-1],x.split('\t')[2]))
+# print("--- Query Last Word In: %s minutes ---" % round(((time.time() - start_time)/60),2))
+# df_all['word_in_title'] = df_all['product_info'].map(lambda x:str_common_word(x.split('\t')[0],x.split('\t')[1]))
+# df_all['word_in_description'] = df_all['product_info'].map(lambda x:str_common_word(x.split('\t')[0],x.split('\t')[2]))
+# df_all['ratio_title'] = df_all['word_in_title']/df_all['len_of_query']
+# df_all['ratio_description'] = df_all['word_in_description']/df_all['len_of_query']
+# df_all['attr'] = df_all['search_term']+"\t"+df_all['brand']
+# df_all['word_in_brand'] = df_all['attr'].map(lambda x:str_common_word(x.split('\t')[0],x.split('\t')[1]))
+# df_all['ratio_brand'] = df_all['word_in_brand']/df_all['len_of_brand']
+# df_brand = pd.unique(df_all.brand.ravel())
+# d={}
+# i = 1000
+# for s in df_brand:
+#     d[s]=i
+#     i+=3
+# df_all['brand_feature'] = df_all['brand'].map(lambda x:d[x])
+# df_all['search_term_feature'] = df_all['search_term'].map(lambda x:len(x))
+# df_all.to_csv('feature_sample.csv')
+df_all = pd.read_csv('feature_sample.csv', encoding="ISO-8859-1", index_col=0)
 df_train = df_all.iloc[:num_train]
 df_test = df_all.iloc[num_train:]
 id_test = df_test['id']
@@ -184,7 +213,7 @@ X_test = df_test[:]
 print("--- Features Set: %s minutes ---" % round(((time.time() - start_time)/60),2))
 
 rfr = RandomForestRegressor(n_estimators = 500, n_jobs = 1, random_state = 2016, verbose = 1)
-tfidf = TfidfVectorizer(ngram_range=(1, 1), stop_words='english')
+tfidf = TfidfVectorizer(ngram_range=(2, 2), stop_words='english')
 tsvd = TruncatedSVD(n_components=10, random_state = 2016)
 clf = pipeline.Pipeline([
         ('union', FeatureUnion(
